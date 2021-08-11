eb0ba3924ee7e94035fbf26421e29adfaea32d34
crazyPythonLover1 <mainulislamfaruqi@gmail.com>
2021-08-11 10:47:54 +0600

    Dashboard status-card

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,9 +1,30 @@
 import React from 'react'
+import statusCard from '../assets/JsonData/status-card-data.json'
+import StatusCard from '../components/status-card/StatusCard'
+
 
 const Dashboard = () => {
     return (
         <div>
-            Dashboard
+            <h2 className="page-header">Dashboard</h2>
+            <div className="row">
+                <div className="col-6">
+                    <div className="row">
+                        {
+                            statusCard.map((item,index) => (
+                                <div className="col-6" key={index}>
+                                    {/* {item.title} */}
+                                    <StatusCard
+                                        icon={item.icon}
+                                        count={item.count} 
+                                        title={item.title}
+                                    />
+                                </div>
+                            ))
+                        }
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
